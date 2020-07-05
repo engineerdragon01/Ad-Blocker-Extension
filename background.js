d1f9e6ac6115d20cc11f30c51b158ea0eab3e058
Adam Chois <dragonboy174@gmail.com>
2020-07-05 15:36:12 -0700

    Get ready to add more sites to block ads from

--- background.js
+++ background.js
@@ -5,6 +5,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     };
   },
   // can specify more subdomains of advertisements here to block
+  // look at cookies to find more sites to block
   { urls: ["*://*.doubleclick.net/"] },
   ["blocking"]
 );
